b7c13eeaff26969c44bc703d60f2ef190178a86a
eddy.claros <eddyclarosc@gmail.com>
2022-02-15 11:41:38 -0400

    primer commit

---
+++ src/pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <q-page padding>
+        <h1>hola about</h1>
+        <h6 class="text-primary bg-yellow-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eius amet facilis eaque totam quasi repellendus minus a qui tempora facere, quisquam quibusdam eos commodi tempore. Saepe recusandae praesentium minus?</h6>
+        <h6 class="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eius amet facilis eaque totam quasi repellendus minus a qui tempora facere, quisquam quibusdam eos commodi tempore. Saepe recusandae praesentium minus?</h6>
+        <h6 class="text-accent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eius amet facilis eaque totam quasi repellendus minus a qui tempora facere, quisquam quibusdam eos commodi tempore. Saepe recusandae praesentium minus?</h6>
+        <h6 class="text-lime-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eius amet facilis eaque totam quasi repellendus minus a qui tempora facere, quisquam quibusdam eos commodi tempore. Saepe recusandae praesentium minus?</h6>
+        <h6 class="text-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eius amet facilis eaque totam quasi repellendus minus a qui tempora facere, quisquam quibusdam eos commodi tempore. Saepe recusandae praesentium minus?</h6>
+
+    </q-page>
+</template>
